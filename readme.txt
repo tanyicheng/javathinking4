79cbd8d9cf544bafb226b95aef0ec341ff1d8796
yicheng.tan <yicheng.tan@seraphim-energy.com>
2020-05-16 21:47:33 +0800

    feat: 简单封装io流拷贝文件

--- readme.txt
+++ readme.txt
@@ -5,3 +5,6 @@ fixme-3 有疑惑的
 # 标记demo的级别
 TODO-1 很好的例子
 TODO-2 还行
+
+
+指定project -> modules jdk1.5
--- src/main/0study/io/InputStreamDemo.java
+++ src/main/0study/io/InputStreamDemo.java
@@ -1,23 +1,27 @@
 package io;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * IO流操作步骤
  * 1、创建源
  * 2、选择流
- * 3、操作（读、写）
+ * 3、操作（读）
  * 4、释放资源
  *
  * @Author created by barrett in 2020/5/14 23:05
  */
-public class BaseIO {
+public class InputStreamDemo {
 
     public static void main(String[] args) {
-        test2();
+        test3();
 
     }
 
+    //基础流出
     static void test1() {
         //源头：文件路径
         File file = new File("readme.txt");
@@ -41,6 +45,7 @@ public class BaseIO {
         }
     }
 
+    //标准写法
     static void test2() {
         File url = new File("io.txt");
 
@@ -50,6 +55,36 @@ public class BaseIO {
             int read;
             while ((read = is.read()) != -1) {
                 System.out.print((char) read);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * 使用字节
+     * @Author created by barrett in 2020/5/15 22:53
+     */
+    static void test3() {
+        File url = new File("io.txt");
+
+        InputStream is = null;
+        try {
+            is = new FileInputStream(url);
+            byte[] flush = new byte[5];//缓冲容器
+            int len;//接收长度
+            while ((len = is.read(flush)) != -1) {
+                //字符数组->字符串解码的过程，传入实际长度（避免空数据    ）
+                String s = new String(flush,0,len);
+                System.out.print(s);
             }
         } catch (IOException e) {
             e.printStackTrace();
